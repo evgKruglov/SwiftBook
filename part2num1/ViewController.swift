b3411864d4e98d95eeb74e0345010327375a777f
Evgeniy Kruglov <evgkukharenko@gmail.com>
2022-07-23 11:41:50 +0300

    Initial Commit

---
+++ part2num1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  part2num1
+//
+//  Created by Евгений Кухаренко on 23.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
