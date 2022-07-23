6b94e6e4f5bfd2875302a42e5b30664f375429ff
Evgeniy Kruglov <evgkukharenko@gmail.com>
2022-07-23 13:16:40 +0300

    changeLabel

--- part2num1/ViewController.swift
+++ part2num1/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
     
+
+    @IBOutlet var changeLabel: UILabel!
+    
+    @IBOutlet var changeButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        changeLabel.isHidden = true
+        changeLabel.layer.cornerRadius = 10
     }
 
     
+    @IBAction func changeLabel(_ sender: Any) {
+        if changeLabel.isHidden {
+            changeLabel.isHidden = false
+            changeButton.setTitle("ON", for: .normal)
+        } else {
+            changeLabel.isHidden = true
+            changeButton.setTitle("OFF", for: .normal)
+        }
+    }
 }
 
